be9246e0f032a9ab1d3d251526bfac330ee46b0b
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-15 01:35:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,24 @@
-makeArray(array)
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 0) {
-      array.push("asdf")
-    } else {
-      array.push("`I am ${i} strange loops.`")
-    }
-    //myArray.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+    
   }
+  return array;
 }
 
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+  do {
+    (array.pop()
+  } while (maybeTrue());
+  
+}
